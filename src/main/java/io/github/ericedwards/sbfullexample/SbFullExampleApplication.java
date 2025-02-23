3f448c4571b5ab60f982473c4f497a18604c02d4
Eric Edwards <ericedwards37@gmail.com>
2025-02-22 23:11:17 -0500

    Starting Spring Boot project

---
+++ src/main/java/io/github/ericedwards/sbfullexample/SbFullExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.ericedwards.sbfullexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFullExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFullExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ericedwards/sbfullexample/SbFullExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ericedwards.sbfullexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFullExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
